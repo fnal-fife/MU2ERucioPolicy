a8a78701e2f22a8f140f2d570569a77788edd764
Brandon White <bjwhite@fnal.gov>
2024-03-20 13:29:49 -0600

    Add version 34 support.

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from .path_gen import construct_surl_mu2e
 from .lfn2pfn import lfn2pfn_MU2E
 
-SUPPORTED_VERSION = ["1.29", "1.30", "1.31", "32", "33"]
+SUPPORTED_VERSION = ["1.29", "1.30", "1.31", "32", "33", "34"]
 
 def get_algorithms():
     return { 'lfn2pfn': { 'MU2E': lfn2pfn_MU2E }, 'surl': { 'mu2e': construct_surl_mu2e } }
